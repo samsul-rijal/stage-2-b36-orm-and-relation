d89cbb4ce5f8ca06446640463249a2c52106e570
Your Name <samsulr99@gmail.com>
2022-07-06 09:47:26 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -20,8 +20,49 @@ exports.addUsers = async (req, res) => {
 
 exports.getUsers = async (req, res) => {
     // code here
+    try {
+
+        const data = await user.findAll()
+
+        res.send({
+            status: "success",
+            data: {
+                user: data
+            }
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: 'failed',
+            message: 'Server Error'
+        })
+    }
 }
 
 exports.getUser = async (req, res) => {
     // code here
+    try {
+
+        const {id}  = req.params
+
+        const data = await user.findAll({
+            where: {id}
+        })
+
+        res.send({
+            status: "success",
+            data: {
+                user: data
+            }
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.send({
+            status: 'failed',
+            message: 'Server Error'
+        })
+    }
+
 }
